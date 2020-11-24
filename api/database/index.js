67790aac5a7aae4774a6ea3f37bd3fae93e6e6ea
Rafael Sumitani <rafael.sumitani@gmail.com>
2020-11-23 22:59:45 -0300

    Configuração database

---
+++ api/database/index.js
@@ -0,0 +1,14 @@
+const { Sequelize } = require('sequelize');
+const config = require('config');
+
+const db = new Sequelize(
+    config.get('mysql.database'),
+    config.get('mysql.user'),
+    config.get('mysql.password'),    
+    {
+        host: config.get('mysql.host'),
+        dialect: 'mysql'
+    }
+);
+
+module.exports = db;
