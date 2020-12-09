d163d14c590652b13ad9947e54f288f3d4b98c80
Rafael Sumitani <rafael.sumitani@gmail.com>
2020-12-09 20:05:05 -0300

    Configurando React e Webpack

---
+++ app/webpack.config.js
@@ -0,0 +1,40 @@
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        path: __dirname + '/public',
+        filename: './app.js',
+    },
+    devServer: {
+        port: 8080,
+        contentBase: './public',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+            modules: __dirname + '/node_modules'
+        }
+    },
+    plugins: [
+        new ExtractTextPlugin('app.css')
+    ],
+    module: {
+        rules: [{
+            test: /.js[x]?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['@babel/preset-env', '@babel/preset-react'],
+                plugins: ['transform-object-rest-spread']
+            }
+        }, {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }, {
+            test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
+            use: 'file-loader'
+        }]
+    }
+}
