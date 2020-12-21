921e644c4302d98d0358f0c82c2347ce7af2d844
Rafael Sumitani <rafael.sumitani@gmail.com>
2020-12-21 16:22:45 -0300

    Cadastro (inicial) de Pokémon

--- app/src/main/App.jsx
+++ app/src/main/App.jsx
@@ -4,14 +4,25 @@ import 'modules/jquery/dist/jquery.min.js';
 import 'modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import React from 'react';
-import Routes from './Routes';
+import { Route, 
+    HashRouter as Router, 
+    Switch } from 'react-router-dom';
 import Navbar from '../templates/Navbar';
+import PokemonList from '../pages/PokemonList';
+import PokemonPage from '../pages/PokemonPage';
+import PokemonForm from '../pages/PokemonForm';
 
 export default function App(props) {
     return (
         <div className="container">
-            <Navbar/>
-            <Routes/>
+            <Router>
+                <Navbar/>
+                <Switch>
+                    <Route exact path='/' component={ PokemonList }/>
+                    <Route path='/pokemon/:number' component={ PokemonPage }/>
+                    <Route path='/registerPokemon' component={ PokemonForm }/>
+                </Switch>
+            </Router>
         </div>
     );
 }
--- app/src/templates/Navbar.jsx
+++ app/src/templates/Navbar.jsx
@@ -1,6 +1,8 @@
 import './Navbar.css';
 
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import PokemonLogo from '../../public/images/pokemon-logo.png';
 
 export default function Navbar(props) {
@@ -8,19 +10,25 @@ export default function Navbar(props) {
         <div className="navbar-wrapper">
             <nav className="navbar navbar-expand-lg navbar-dark bg-red">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">
+                    <Link to="/" className="navbar-brand">
                         <img className="brand-image" src={PokemonLogo}/>
-                    </a>
+                    </Link>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="navbar-collapse collapse" id="navbar">
                         <ul className="nav-list navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Pokédex</a>
+                                <Link className="nav-link active" to="/">Pokédex</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link active" href="#">Cadastrar Pokémon</a>
+                                <Link className="nav-link active" 
+                                    to={{
+                                        pathname: "/registerPokemon",
+                                        state: {
+                                            isRegister: true
+                                        }
+                                    }}>Cadastrar Pokémon</Link>
                             </li>
                         </ul>
                     </div>
